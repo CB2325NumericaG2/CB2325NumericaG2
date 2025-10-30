b0d6e69954bd6f183cc02f5310648ac833e3b1ce
Gabrielle Silva <al.gabrielle.silva@impatech.edu.br>
2025-10-30 14:09:30 -0300

    Conversão: método dos trapézios para método dos retângulos. Ajuste necessário

--- src/integracao_numerica/integracao_curva.py
+++ src/integracao_numerica/integracao_curva.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sympy
 
-def riemann_integ_trapezio(f_string, a, b, n):
+def riemann_integ_trapezio(f_string, a, b, n, ponto_corte):
 
     # Definição de x para computação simbólica
     x_simbolico = sympy.symbols('x') 
@@ -21,22 +21,23 @@ def riemann_integ_trapezio(f_string, a, b, n):
     area = 0
     for i in range(n):
 
-        # Definindo vertices do trapezio
+        # Definindo vertices do retangulo
         x_inicio = a + i * variacao
         y_inicio = f(x_inicio)
-
         x_fim = x_inicio + variacao
         y_fim = f(x_fim)
 
-        x_vertices = [x_inicio, x_inicio, x_fim, x_fim]
-        y_vertices = [0, y_inicio, y_fim, 0]
+        # Definindo altura do retângulo
+        x_altura = x_inicio + ponto_corte * (x_fim - x_inicio)
+        y_altura = f(x_altura)
         
-        # Calculando a area do trapezio e somando a area final
-        area_trapezio = ((y_inicio + y_fim) * variacao) / 2
-        area += area_trapezio
+        # Calculando a area do retangulo e somando a area final
+        area_retangulo = y_altura * variacao
+        area += area_retangulo
         
         # Preenchendo os trapézios 
-        ax.fill(x_vertices, y_vertices, color='gray', edgecolor='black', alpha=0.5)
+        ax.bar(x_inicio, y_inicio, width = variacao, align = 'edge', \
+                color = 'gray', edgecolor = 'black', alpha = 0.5)
 
 
 
@@ -51,11 +52,11 @@ def riemann_integ_trapezio(f_string, a, b, n):
     ax.set_ylabel("f(x)")
     ax.legend()
     ax.grid(False)
-    ax.set_xlim([a, b])
     
     # Definindo os limites do gráfico
     min_y = min(0, np.min(y_curve)) 
     max_y = np.max(y_curve)
+    ax.set_xlim([a, b])
     ax.set_ylim([min_y - max_y*0.05, max_y * 1.1])
     
     plt.show()
@@ -65,8 +66,13 @@ string = input('Digite a função:')
 valor_a = input('Digite a:')
 valor_b = input('Digite b:')
 valor_n = input('Digite n:')
+valor_corte = input('Digite 0 para Soma de Riemann pela Esquerda,' \
+                            '1 para Soma de Riemann pela Direita,'  \
+                            '0.5 para Soma de Riemann pelo Centro:')
 
 a_num = float(valor_a)
 b_num = float(valor_b)
 n_int = int(valor_n)
-riemann_integ_trapezio(string, a_num, b_num, n_int)
+corte_num = float(valor_corte)
+
+riemann_integ_trapezio(string, a_num, b_num, n_int, corte_num)

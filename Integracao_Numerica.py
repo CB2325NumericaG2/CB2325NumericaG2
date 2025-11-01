d0954bb797b1e420b3595fad6ea70fc8462bce85
Italo <al.italo.souza@impatech.edu.br>
2025-11-01 10:27:35 -0300

    Remove o parâmetro 'plot' da função 'integral_real' e cria o arquivo 'visualicações.ipynb', onde é feita a função 'plot_func_trapezio' que cria uma visualização do gráfico de uma função f(x) e da aproximação da área sob ele

--- Integracao_Numerica.py
+++ Integracao_Numerica.py
@@ -3,18 +3,17 @@ import numpy as np
 from functools import reduce
 from typing import Callable
 
-def integral_trapézio(func:Callable[[float],float], pi:float, pf:float, n:int, plot:bool=False) -> float:
+def integral_trapézio(func:Callable[[float],float], pi:float, pf:float, n:int) -> float:
     """
     Aplica o método da Regra do Trapézio Composta para encontrar uma aproximação da integral de f(x) 
     no intervalo [pi, pf]. Para isso, o intervalo é dividido em n subintervalos de tamanho igual e em cada 
-    deles é construído um trapézio que aproximará a área sob o gráfico de f(x) naquela parte.
+    um deles é construído um trapézio que aproximará a área sob o gráfico de f(x) naquela parte.
 
     Args:
         func (Callable[[float], float]): Função f(x).
         pi (float): Limite inferior do intervalo.
         pf (float): Limite superior do intervalo.
         n (int): Número de subdivisões trapezoidais.
-        plot (float, opcional): Visualização do gráfico de f(x) com n subdivisões trapezoidais e a área sombreada.
 
     Returns:
         float: Aproximação da integral de f(x) = 0.
@@ -36,33 +35,11 @@ def integral_trapézio(func:Callable[[float],float], pi:float, pf:float, n:int, 
     
     # Cálculo da integral com regra do trapézio
     area = reduce(lambda acc, i: acc + (func(pontos[i]) + func(pontos[i+1])) * h / 2, range(n), 0)
-
-    # Plotagem (opcional)
-    if plot:
-        # Gerar pontos para desenhar a curva da função
-        x = np.linspace(pi, pf, 400)
-        y = func(x)
-
-        plt.figure(figsize=(10,6))
-        plt.plot(x, y, 'b', label='f(x)')  # Curva da função
-        plt.axhline(0, color='black', linewidth=1)
-
-        # Desenhar trapézios
-        for i in range(n):
-            x_trap = [pontos[i], pontos[i], pontos[i+1], pontos[i+1]]
-            y_trap = [0, func(pontos[i]), func(pontos[i+1]), 0]
-            plt.fill(x_trap, y_trap, 'orange', alpha=0.3, edgecolor='r')
-
-        plt.title(f"Método dos Trapézios (n={n}) | Área Aprox. = {area:.2f}")
-        plt.xlabel("x")
-        plt.ylabel("f(x)")
-        plt.legend()
-        plt.grid(True)
-        plt.show()
     return area
 
 # Teste unitário
+
 if __name__ == "__main__":
     f = lambda x: np.sin(x)**2
-    resultado = integral_trapézio(f, -10, 10, 50, plot=True)
+    resultado = integral_trapézio(f, -10, 10, 50)
     print(f"Integral aproximada = {resultado}")

e2d84b9fbb64260ada7155f6665f2074885880b6
samueleite <al.samuel.leite@impatech.edu.br>
2025-11-01 09:52:32 -0300

    adicionando testes

--- tests/test_raizes_de_funcoes.py
+++ tests/test_raizes_de_funcoes.py
@@ -1,6 +1,10 @@
 import pytest
 import math
-from . import bissecao, newton_raphson, secante  
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
+from raizes_de_funcoes import bissecao, newton_raphson, secante
 
 def test_bissecao():
     f = lambda x: x**2 - 2
@@ -10,7 +14,7 @@ def test_bissecao():
 def test_newton_raphson():
     f = lambda x: x**2 - 2
     df = lambda x: 2*x
-    aproximacao = newton_raphson(f, df, x0=1, tol=1e-7, max_iter=100)
+    aproximacao = newton_raphson(f, x0=1, tol=1e-7, max_iter=100, df=df)
     assert abs(aproximacao - math.sqrt(2)) < 1e-7
 
 def test_secante():

52231cf0599f42dc5ddd2629b28a99729a98605b
GabiOgido <al.gabriela.ogido@impatech.edu.br>
2025-11-09 15:32:13 -0300

    Adiciona mais testes para interpolacao polinomial

--- src/interpolacao/__init__.py
+++ src/interpolacao/__init__.py
@@ -1,5 +1,5 @@
 from .linear import interpolacao_linear as linear
 from .linear_partes import interpolacao_linear_partes as linear_partes
-from .interpolacao_polinomial import PolinomioInterpolador
+from .interpolacao_polinomial import PolinomioInterpolador as polinomial
 
-__all__ = ["linear_partes", "linear", "PolinomioInterpolador"]
+__all__ = ["linear_partes", "linear", "polinomial"]

6739f40e204b3b9025860943721a6164c20c1b13
samueleite <al.samuel.leite@impatech.edu.br>
2025-11-12 11:01:38 -0300

    alteração do nome dos métodos das raizes

--- src/raizes/__init__.py
+++ src/raizes/__init__.py
@@ -1,5 +1,5 @@
-from .bissecao import bissecao as metodo_bisseccao
-from .newton_raphson import newton_raphson as metodo_newton_raphson
-from .secante import secante as metodo_secante
+from .bissecao import bisseccao
+from .newton_raphson import newton_raphson
+from .secante import secante
 
-__all__ = ["metodo_bisseccao", "metodo_newton_raphson", "metodo_secante"]
+__all__ = ["bisseccao", "newton_raphson", "secante"]
